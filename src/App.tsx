521837dd7081fae03135999c36be00bd5a65a2e4
LesterLien <lesterlien11@gmail.com>
2025-04-22 01:42:07 -0700

    initial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import './styles/App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      Learn React
+        <NavBar/>
     </div>
   );
 }
