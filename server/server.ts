9db16629002d41f46c977501e4309cdf3c6a45ec
LesterLien <lesterlien11@gmail.com>
2025-04-27 01:26:34 -0700

    organize the birthdays in categories for backend

--- server/server.ts
+++ server/server.ts
@@ -18,13 +18,43 @@ app.get('/user-age', async (req, res) => {
         .from('users')
         .select('birth_date');
 
-    if (error || !users) {
-        console.error('Supabase error:', error?.message);
-        return res.status(500).json({ error: error?.message });
+    if (error) {
+        console.error('Supabase error:', error.message);
+        return res.status(500).json({ error: error.message });
     }
 
+    if(!users) {
+        return res.status(500).json({error: 'User data is empty'});
+    }
 
-    res.json(users);
+    const currentDay = new Date();
+    
+    const user_birthdays: Record<string, number> = {};
+
+    users
+        .filter((user: { birth_date: string | null }) => user.birth_date !== null)
+        .forEach((user: { birth_date: string }) => {
+            const user_birth_date = new Date(user.birth_date);
+            
+            let age = currentDay.getFullYear() - user_birth_date.getFullYear();
+            const month = currentDay.getMonth() - user_birth_date.getMonth();
+
+
+            if (month < 0 || (month === 0 && currentDay.getDate() < user_birth_date.getDate())) {
+                age--;
+            }
+
+            const ageLabel = Math.floor(age / 2) * 2;
+            const ageRangeLabel = `${ageLabel}-${ageLabel + 1}`;
+
+            if (user_birthdays[ageRangeLabel]) {
+              user_birthdays[ageRangeLabel]++;
+            } else {
+              user_birthdays[ageRangeLabel] = 1;
+            }
+        });
+
+    res.json(user_birthdays);
 });
 
 
